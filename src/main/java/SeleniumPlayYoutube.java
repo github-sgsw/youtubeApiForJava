6528668d3308e2ad1d95d677d77bdfa689b374e0
“sugi” <p20prosugi@gmail.com>
2023-02-07 03:05:52 +0900

    #2　スキップ機能も搭載できた！

--- src/main/java/SeleniumPlayYoutube.java
+++ src/main/java/SeleniumPlayYoutube.java
@@ -1,26 +1,31 @@
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chrome.ChromeDriverService;
 
-import java.util.HashMap;
 import java.util.Scanner;
-
-import static java.lang.Thread.sleep;
 
 public class SeleniumPlayYoutube {
     YoutubeSearchResult youtube = new YoutubeSearchResult();
     Scanner sc = new Scanner(System.in);
     WebDriver driver = new ChromeDriver();
     public void createDriver() {
-
         youtube.requestApi();
         System.out.print("再生する動画id -->  ");
         String playId = sc.next();
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+        // 動画idで直検索
         driver.get("https://www.youtube.com/watch?v=" + playId);
+        /**
+         * 1. 広告スキップなし→そのまま動画スタート...5秒くらいループさせて終わり
+         * 2. 5 ~ 6秒後にスキップボタン表示→動画スタート...広告中無限ループ時にスキップボタンが有効になったらクリックして飛ばす
+         * 3. スキップできない広告→5 ~ 6秒後にスキップボタン表示(2回目の広告)...故意的にこれ発生させれないから対応できてるかわからない。
+         *    ２回目でxパス変わっていたら無理そう
+         */
+        while (driver.findElement((By.xpath("//*[@id=\"movie_player\"]/div[4]"))).isDisplayed()) {
+            System.out.println("広告検知");
+            var adArea = driver.findElement(By.xpath("//*[@id=\"skip-button:6\"]/span/button"));
+            if (adArea.isDisplayed()) adArea.click();
+        }
+        System.out.println("広告なし");
     }
 }
--- src/main/java/YoutubeSearchResult.java
+++ src/main/java/YoutubeSearchResult.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Optional;
+
 import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -15,7 +14,6 @@ public class YoutubeSearchResult {
     Scanner sc = new Scanner(System.in);
     JsonNode jsonNode;
     ObjectMapper mapper = new ObjectMapper();
-    //HashMap<String, String> resultInfo = new HashMap<>();
 
     public void requestApi() {
         System.out.print("検索ワード -->  ");
@@ -27,9 +25,8 @@ public class YoutubeSearchResult {
                     "?key=AIzaSyDXA3tm85n2YcvL3RRCdXRZ4R6pHTh0FJU" +
                     "&type=video" +
                     "&part=snippet" +
-                    "&order=viewCount" +
                     "&maxResults=5" +
-                    "&q=" + searchQuery ;
+                    "&q=" + searchQuery;
             var request = HttpRequest.newBuilder().uri(URI.create(url)).build();
             var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
@@ -39,18 +36,18 @@ public class YoutubeSearchResult {
                 String id = json.get("id").get("videoId").asText();
                 String title = json.get("snippet").get("title").asText();
                 System.out.println("id -> " + id + "\n" +  "title -> " + title);
-                //resultInfo.put(id, title);
             }
+
         } catch(IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
-        //return resultInfo;
     }
 }
 /**
- * とりあえずなんとなくmapに格納したけど
+ * とりあえずなんとなくidとtitleはmapに格納したけど
  * コンソールに表示させたidコピペしてchromeDriver起動するから格納処理いらんくね感でできた；；
+ * ↑map関連のコメントアウトもろもろ消した
+ *
+ * 返却されるjson確認用
+ * https://www.googleapis.com/youtube/v3/search?key=AIzaSyDXA3tm85n2YcvL3RRCdXRZ4R6pHTh0FJU&type=video&part=snippet&order=viewCount&maxResults=5&q=
  */
-
-// https://www.googleapis.com/youtube/v3/search?key=AIzaSyDXA3tm85n2YcvL3RRCdXRZ4R6pHTh0FJU&type=video&part=snippet&order=viewCount&maxResults=5&q=
