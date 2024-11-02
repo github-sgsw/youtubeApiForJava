4d1d940b999e30080986dca2f6acf1fb85e9e3f2
sgsw <p20prosugi@gmail.com>
2024-11-02 17:01:20 +0900

    2年前の自分直接apiKey書いてたの怖いー

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        private static final String INIT_FILE_PATH = "resourse/.env";
-        private static final Properties properties;
+        final String INIT_FILE_PATH = "./src/main/resources/.env";
+        String apiKey = "";
         try {
             // 設定ファイルを読み取る処理
             Properties property = new Properties();
-            property.load(new FileInputStream(PROPERTY_FILE));
+            property.load(new FileInputStream(INIT_FILE_PATH));
 
             // 読み取ったものから実際に設定値を取り出す処理
-            final String apiKey = property.getProperty("youtubeApiKey");
+            apiKey = property.getProperty("youtubeApiKey");
 
         } catch (IOException e) {
             // ファイルの読み込みエラー
